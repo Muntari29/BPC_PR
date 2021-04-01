bde13e179cd438256243cf23e89e9b278577e7d1
Muntari29 <muntrock@gmail.com>
2021-04-01 19:05:19 +0900

    feat : Add Purchased book 기능 구현 및 테스트 완료

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,9 +1,10 @@
 import { UploadBookDto } from './dto/upload-book.dto';
-import { Injectable, BadRequestException, NotFoundException, Delete } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException, Delete, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Connection, Like, Repository } from 'typeorm';
+import { Connection, Like, Repository, getRepository, getConnection, QueryFailedError } from 'typeorm';
 import { BookRepository } from './book.repository';
 import { Book } from './entities/book.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class BookService {
@@ -73,6 +74,56 @@ export class BookService {
             return null;
         } else {
             return bookData;
+        }
+    }
+
+    // Add BPC
+    async addBpc(req: any, bookTitle: string): Promise<any>{
+        const userId = req.user.id;
+        const bookId = await getRepository('Book')
+        .createQueryBuilder('book')
+        .where({title:bookTitle})
+        .getRawOne()
+
+        // bookId validation
+        if (!bookId){
+            throw new NotFoundException('Not_found_book');
+        }
+
+        // userId validation
+        const user = await getRepository('User')
+        .createQueryBuilder('user')
+        .where({id: userId})
+        .getRawOne()
+
+        let userdata = new User();
+        userdata.id = user.user_id;
+        userdata.real_name = user.user_real_name;
+        userdata.nick_name = user.user_nick_name;
+        userdata.email = user.user_email;
+        userdata.password = user.user_password;
+        userdata.image_url = user.user_image_url;
+        
+        let bookBpc = new Book();
+        bookBpc.id = bookId.book_id;
+        bookBpc.title = bookId.book_title;
+        bookBpc.image_url = bookId.book_image_url;
+        bookBpc.contents = bookId.book_contents;
+        bookBpc.datetime = bookId.book_datetime;
+        bookBpc.users = [user];
+
+        // QueryFailedError validation (Duplicate PK)
+        try{
+            await getConnection()
+            .createQueryBuilder()
+            .relation(Book, "users")
+            .of(bookBpc)
+            .add(userdata);
+
+            return 'Success_Add_book';
+        } catch(e){
+            console.log('QueryFaildError');
+            throw new BadRequestException('QueryfaildError_Duplicate PK');
         }
     }
 
